d4b70ddf7558822bc82c2971bb6bb456cb6db85a
danrley0595 <danrleycramos@gmail.com>
2026-02-05 19:29:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ somatoria_receitas = (salario + receita_outros)
 saldo_atual = (somatoria_receitas - somatoria_desp)
 
 
-#Imprime condição do saldo atual sem a meta
+# Faz a comparação do saldo e imprime condição do saldo atual sem a meta
 print("Situação do saldo atual sem o valor da meta!\n")
-if saldo_atual < 0.0:
-    print(f"Saldo Negativo, você está no vermelho!")
-elif saldo_atual == 0.0:
-    print("Saldo zero, no limite")
+if saldo_atual < 0.00:
+    print(f"Saldo Negativo {saldo_atual}, você está no vermelho!")
+elif saldo_atual == 0.00:
+    print(f"Saldo zero, no limite")
 else:
-    print("Saldo Positivo, você está no Azul")
+    print(f"Saldo Positivo {saldo_atual}, você está no Azul")
 
 #Apresenta Totais
 print(f"Valor total receitas: R$ {somatoria_receitas:.2f}")
@@ -42,21 +42,21 @@ valor_mensal_meta = float(input("Informe o valor mensal que quer guardar:"))
 qtd_meses_meta = int(valor_meta // valor_mensal_meta)
     
 # Apreseta Meses  para atingimento para meta
-print(f"A sua meta de {valor_meta} será atingida em {qtd_meses_meta} meses.")
+print(f"\nA sua meta de {valor_meta} será atingida em {qtd_meses_meta} meses.\n")
 
 #Adiciona o valor da meta as despesas
 somatoria_desp = (somatoria_desp + valor_mensal_meta)
 saldo_atual = (somatoria_receitas - somatoria_desp)
 
-#Imprime condição do saldo atual com a meta
-print("Situação do saldo atual sem o valor da meta!\n")
-if saldo_atual < 0.0:
-    print(f"Saldo Negativo, você está no vermelho!")
-elif saldo_atual == 0.0:
-    print("Saldo zero, no limite")
+# Faz a comparação do saldo e Imprime condição do saldo atual com a meta
+print("Situação do saldo atual com o valor da meta mensal!\n")
+if saldo_atual < 0.00:
+    print(f"Saldo Negativo de {saldo_atual}, você está no vermelho!")
+elif saldo_atual == 0.00:
+    print(f"Saldo zero, no limite")
 else:
-    print("Saldo Positivo, você está no Azul")
+    print(f"Saldo Positivo de {saldo_atual}, você está no Azul")
 
 #Apresenta Totais com meta
 print(f"Valor total receitas: R$ {somatoria_receitas:.2f}")
-print(f"Valor total despesas: R$ {somatoria_desp:.2f}\n")
+print(f"Valor total despesas com a meta: R$ {somatoria_desp:.2f}\n")
