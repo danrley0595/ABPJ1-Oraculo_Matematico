2e08b8c8ae43d6a213477f9f63ba60b8e3d41b29
danrley0595 <danrleycramos@gmail.com>
2026-02-05 20:09:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+print("ABPJ1 - CÁLCULO FINANCEIRO\n")
+
 # Entradas receitas
 salario = float(input("Informe seu Sálario:"))
 receita_outros = float(input("Informe o valor de outras receitas:"))
@@ -20,43 +22,51 @@ somatoria_receitas = (salario + receita_outros)
 #Calculo saldo atual
 saldo_atual = (somatoria_receitas - somatoria_desp)
 
-
 # Faz a comparação do saldo e imprime condição do saldo atual sem a meta
 print("Situação do saldo atual sem o valor da meta!\n")
 if saldo_atual < 0.00:
-    print(f"Saldo Negativo {saldo_atual}, você está no vermelho!")
+    print(f"Saldo Negativo R$ {saldo_atual:.2f}, você está no vermelho!")
 elif saldo_atual == 0.00:
     print(f"Saldo zero, no limite")
 else:
-    print(f"Saldo Positivo {saldo_atual}, você está no Azul")
+    print(f"Saldo Positivo R$ {saldo_atual:.2f}, você está no Azul")
 
 #Apresenta Totais
 print(f"Valor total receitas: R$ {somatoria_receitas:.2f}")
 print(f"Valor total despesas: R$ {somatoria_desp:.2f}\n")
 
-#Entradas meta
-valor_meta = float(input("Informe o valor da meta que deseja alcançar:"))
-valor_mensal_meta = float(input("Informe o valor mensal que quer guardar:"))
+#Condição para calcular a meta s ou n
+cond_meta = str(input("Deseja calcular a meta(s para SIM ou n para NAO):"))
 
-#Calculo meta 
-qtd_meses_meta = int(valor_meta // valor_mensal_meta)
-    
-# Apreseta Meses  para atingimento para meta
-print(f"\nA sua meta de {valor_meta} será atingida em {qtd_meses_meta} meses.\n")
+if cond_meta == "s":
 
-#Adiciona o valor da meta as despesas
-somatoria_desp = (somatoria_desp + valor_mensal_meta)
-saldo_atual = (somatoria_receitas - somatoria_desp)
+    #Entradas meta
+    valor_meta = float(input("\nInforme o valor da meta que deseja alcançar:"))
+    valor_mensal_meta = float(input("Informe o valor mensal que quer guardar:"))
 
-# Faz a comparação do saldo e Imprime condição do saldo atual com a meta
-print("Situação do saldo atual com o valor da meta mensal!\n")
-if saldo_atual < 0.00:
-    print(f"Saldo Negativo de {saldo_atual}, você está no vermelho!")
-elif saldo_atual == 0.00:
-    print(f"Saldo zero, no limite")
+    #Calculo meta 
+    qtd_meses_meta = int(valor_meta // valor_mensal_meta)
+
+    #Adiciona o valor da meta as despesas
+    somatoria_desp = (somatoria_desp + valor_mensal_meta)
+    saldo_atual = (somatoria_receitas - somatoria_desp)
+
+    # Faz a comparação do saldo e Imprime condição do saldo atual com a meta
+    print("\nSituação do saldo atual com o valor da meta mensal!\n")
+
+    if saldo_atual < 0.00:
+        print(f"Saldo Negativo de R$ {saldo_atual:.2f}, você está no vermelho!")
+    elif saldo_atual == 0.00:
+        print(f"Saldo zero, no limite")
+    else:
+        print(f"Saldo Positivo de R$ {saldo_atual:.2f}, você está no Azul")
+
+    # Apreseta Meses  para atingimento para meta
+    print(f"\nA sua meta de R$ {valor_meta:.2f} será atingida em {qtd_meses_meta} meses.")
+
+    #Apresenta Totais com meta
+    print(f"Valor total receitas: R$ {somatoria_receitas:.2f}")
+    print(f"Valor total despesas com a meta: R$ {somatoria_desp:.2f}\n")
 else:
-    print(f"Saldo Positivo de {saldo_atual}, você está no Azul")
+    print("Cálculo realizado com sucesso. Programa finalizado!")
 
-#Apresenta Totais com meta
-print(f"Valor total receitas: R$ {somatoria_receitas:.2f}")
-print(f"Valor total despesas com a meta: R$ {somatoria_desp:.2f}\n")
